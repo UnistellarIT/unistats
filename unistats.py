1cee1471b4c81dd184f1edad8d841fe851ad4e25
pierbintechnologies <104099726+pierbintechnologies@users.noreply.github.com>
2022-08-28 15:32:24 +0200

    add top websites

--- unistats.py
+++ unistats.py
@@ -1,6 +1,7 @@
 # from pydoc_data.topics import topics
 # from turtle import width
 # from types import CellType
+from inspect import stack
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
@@ -33,6 +34,9 @@ bubbles_fr = bubbles_fr[bubbles_fr['Country']=='France']
 bubbles_us = pd.read_csv("bubbles.csv")
 bubbles_us = bubbles_us[bubbles_us['Country']=='United States']
 
+demand_us = pd.read_csv("demandus.csv")
+demand_fr = pd.read_csv("demandfr.csv")
+
 #### SIDEBAR ###
 
 with st.sidebar:
@@ -52,7 +56,7 @@ with st.sidebar:
         data = data_us
         bubbles = bubbles_us
         size_factor = 5
-        
+
     lvl = st.radio(
         label = 'Level of analysis',
         options = (
@@ -87,7 +91,6 @@ with st.sidebar:
 
     height = st.slider('Chart size', 500, 2000, 500)
 
-#### DASHBOARD ###
 
 col1, col2 = st.columns(2)
 
@@ -130,7 +133,6 @@ fig.update_layout(
 with col2:
     st.header("Topics")
     st.plotly_chart(fig)
-
 
 
 # Full table
@@ -204,3 +206,114 @@ fig.update_layout(
 st.plotly_chart(fig)
 
 bubbles[['Topic','Queries per month','CTR','Conversion rate','Sales per year','Definition']]
+
+#### DASHBOARD ###
+
+st.header("Top websites per group")
+
+site_types = {
+    'out of scope':[['imdb','allocine','starcitygames',
+                    'sezane','gala','dragonforms','gerbeaud','jardiner-malin','siriusxm','siriuscom','eclipse','nebuleusebijoux','telescop','latelescop','etoiltour','etoiltours','etoilverif',
+                    'venus','mercurymarine','pluto','issworld','meteor','bluemoonbrewingcompany','observatoryoc',
+                    'wiktionary','merriam-webster','larousse','lerobert','cnrtl','dicocitations',
+                    'timeanddate','almanac','farmersalmanac','calendarr','moongiant','calendrier-365','calendrier-lunaire','ephemeride','ephemeride-jour','very-utile','nouvellelune','calagenda','lalanguefrancaise','synonymo',
+                    'google',
+                    ],'#333333'],
+    'Generalist & Social Media':[['wikipedia','britannica','vikidia','universalis','harvard','stanford',
+                        'reddit','twitter','instagram','facebook','linkedin','youtube','curiositystream','tripadvisor',
+                        'cnn','nytimes','npr','insidehook','usnews','livenation','outdoorproject',
+                        'francetvinfo','ouest-france','lemonde','linternaute','numerama','01net','bfmtv','lepoint','20minutes','familiscope','lesechos',
+                        ],'#767782'],
+    'Specialised Media':[['futura-sciences','universcience','techno-science','sciencesetavenir','presse-citron',
+                        't3','digitalcameraworld','arstechnica','petapixel','on-mag',
+                        'destination-orbite','astrofiles','webastro','astrosurf','le-systeme-solaire','constellation-guide',
+                        'scientificamerican','sciencenewsforstudents','nationalgeographic','constellation','astronomy','skyandtelescope','magazineheaven','skyatnightmagazine','lovethenightsky','spaceflightnow','astrobackyard','nineplanets','earthsky',
+                        'stellarium',#software map
+                        ],'#0808e1'],
+    'Space Organizations & Places':[['nasa','esa','esahubble','cnes','agences-spatiales','hubblesite','moonexpress','aas','amsmeteors','afastronomie','astronomerswithoutborders', #meteor society
+                        'spacex','starlink','blueorigin','virgingalactic',
+                        'enseignementsup-recherche','arxiv','eventhorizontelescope','aanda','si', #si = airandspace.si.edu
+                        'griffithobservatory','kennedyspacecenter','amnh','rmg','adventuresci','intrepidmuseum','mods-museum','spacecenter','visitspacecoast','lowell','sandiegoairandspace','cincinnatiobservatory',
+                        'cite-sciences','cite-espace','parisinfo','cap-sciences','nantes','bordeaux','psl','reims','nantesmetropole','espace-sciences','lyon-france','vaulx-en-velin','saintmichellobservatoire','unistra','forumdepartementaldessciences',
+                        ],'#6dc85a'],
+    'brands':[['unistellaroptics','unistellar','skywatcher','bresser','vaonis','celestron','omegon','meade','bushnell','zhumell'],'#d20623'],
+    'ressellers':[['amazon','telescope','space',
+                'stelvision','medas-instruments','pierro-astro','astroshop','maison-astronomie','maisonastronomie','telescopes-et-accessoires','achat-telescope','promo-optique','laclefdesetoiles','univers-astro','promo-optique','jeulin','exop',
+                'optcorp','telescopicwatch','highpointscientific','levenhuk','bhphotovideo','telescopesplus','buytelescopes','shopatsky','adorama',
+                'natureetdecouvertes','loisirsplaisirs','fnac','cdiscount','carrefour','boulanger',
+                'walmart','tasco','target','costco','bestbuy',
+                'ebay','leboncoin','aliexpress','rakuten','pagesjaunes',],'#95331e'],
+    }
+column1, column2 = st.columns(2)
+bubble_names = ['Space','Planets / moon / sun','Observable objects','Astronomy','Places','Telescopes','Buy Telescopes','Resellers','High-end brands','Unistellar']
+def plot_charts(demand_us,where):
+    for n in bubble_names:
+
+        top20 = demand_us[demand_us['Topics']==n].groupby(['Domain']).sum().sort_values(by=['Traffic'], axis=0, ascending=True, inplace=False, kind='quicksort', na_position='last').drop('Avg. monthly searches', axis=1)
+        top20 = top20.tail(20)
+        top20 = top20.reset_index()
+        top20['site_type']='other'
+
+        fig = go.Figure()
+
+        for t in site_types:
+            top20.loc[top20["Domain"].isin(site_types[t][0]),'site_type'] = t
+
+        fig.add_trace(go.Bar(
+            y=top20[top20.site_type == 'other']["Domain"],
+            x=top20[top20.site_type == 'other']["Traffic"],
+            marker_color='#feb82b',
+            marker_line_color='rgba(0, 0, 0, 0)',
+            name='other',
+            orientation='h'))
+
+        for t in site_types:
+            fig.add_trace(go.Bar(
+                y=top20[top20.site_type == t]["Domain"],
+                x=top20[top20.site_type == t]["Traffic"],
+                marker_color=site_types[t][1],
+                marker_line_color='rgba(0, 0, 0, 0)',
+                name=t,
+                orientation='h'))
+
+
+
+        fig.update_layout(
+            height=500,
+            width=500,
+            title=n + where,
+            plot_bgcolor="#0c0c0e",
+            barmode='stack',
+            )
+        st.plotly_chart(fig)
+
+with column1:
+    plot_charts(demand_us,' - Top Websites - United States')
+
+
+with column2:
+    plot_charts(demand_fr,' - Top Websites - France')
+
+if country == 'United States':
+    fulldomain = demand_us[demand_us.Topics=='Telescopes'].sort_values(by='Traffic',ascending=False)[['Domain','Url','Traffic']]
+elif country == 'France':
+    fulldomain = demand_fr.sort_values(by='Traffic',ascending=False)[['Domain','Url','Traffic']]
+
+st.dataframe(demand_us[demand_us.Topics=='Telescopes'])
+
+agdata1 = fulldomain
+builder1 = GridOptionsBuilder.from_dataframe(agdata1)
+builder1.configure_column("Domain", editable=True)
+grid_opt1 = builder1.build()
+AgGrid(agdata1, theme='dark', gridOptions=grid_opt1)
+
+# Display top keywords
+comment = """
+with column1:
+    for b in bubble_names:
+        st.dataframe(data_us[data_us.bubble == b].head(10)[['Keyword']])
+
+with column2:
+    for b in bubble_names:
+        st.dataframe(data_fr[data_fr.bubble == b].head(10)[['Keyword']])
+"""
